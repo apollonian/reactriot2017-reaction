57e488bf9aed9411fd482fbeb717433220b1bbc1
Rahul Jain <rahul_jain@live.in>
2017-06-26 04:10:59 +0530

    banner position

--- src/layouts/PageLayout/PageLayout.js
+++ src/layouts/PageLayout/PageLayout.js
@@ -6,7 +6,6 @@ import './PageLayout.scss'
 export const PageLayout = ({ children }) => (
   <div className='PageLayout'>
     <div className='PageLayout__Header'>
-      <div id='hackbit-vote-widget' />
       <a href='/'>
         <h1 className='Logo'>
           <span className='Read'>Read</span>
@@ -15,12 +14,6 @@ export const PageLayout = ({ children }) => (
       </a>
       <div className='Pitch'>Search for topics, get trending results, and read them uniquely!</div>
     </div>
-    {/* <IndexLink to='/' activeClassName='page-layout__nav-item--active'>Home</IndexLink>
-    {' · '}
-    <Link to={{ pathname: '/article/asdf' }}
-      activeClassName='page-layout__nav-item--active'>
-        Article
-    </Link> */}
     <div className='page-layout__viewport'>
       {children}
     </div>
